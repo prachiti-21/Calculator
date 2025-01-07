77b5cc7308a956339f967ab6493bf467cbc2fa6a
Prachiti <prachitiagarkar1@gmail.com>
2025-01-07 15:57:32 +0530

    3rd commit

---
+++ subtraction.java
@@ -0,0 +1,10 @@
+class Calculator{
+public static void sum(int x,int y){
+        System.out.println(x+y);
+    }
+public static void subtract(int x,int y) {
+      System.out.println(x-y);
+    }
+public static void main(String[] args){
+   System.out.print("this is a calculator");
+}}
