b6ff03e77930a08e8fef058ead1e179d8e82caa4
Prachiti <prachitiagarkar1@gmail.com>
2025-01-07 15:37:41 +0530

    2nd commit

---
+++ add.java
@@ -0,0 +1,7 @@
+class Calculator{
+public static void sum(int x,int y){
+        System.out.println(x+y);
+    }
+public static void main(String[] args){
+   System.out.print("this is a calculator");
+}}
